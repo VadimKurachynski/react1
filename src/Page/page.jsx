ff244e37da7d53ee7f65888350de2a82894cdc01
VadimKurachynski <kurachynski@gmail.com>
2022-11-02 21:06:57 +0300

    pa

---
+++ src/Page/page.jsx
@@ -0,0 +1,19 @@
+import s from "./page.module.css"
+
+
+const PageStr = () => {
+
+    return (
+        <div className={s.content}>
+            <div>верх</div>
+            <div><button>button1</button><button>button2</button></div>
+            <div></div>
+            <div>низ</div>
+        </div>
+    )
+}
+
+
+
+
+export default PageStr;
