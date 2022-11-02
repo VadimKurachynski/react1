0df4612429d50aeea0842d0fb58cb60b0b41d4f9
VadimKurachynski <kurachynski@gmail.com>
2022-11-02 22:38:41 +0300

    8

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,17 +1,20 @@
 import {legacy_createStore} from "redux";
 
-
+const one = 'one';
 const initialState={
     likes:10
 }
-
-// const reducer=(state=initialState, action)=>{
-//     console.log("reducer----  ", action);
-//     return state;
-// }
-
-
-
-const store=legacy_createStore(reducer);
-
+const reducer=(state=initialState, action)=>{
+    console.log("reducer");
+    switch (action.type) {
+        case one:
+            return{
+                ...state.likes+=1
+            };
+        default:
+            return state;
+    }
+}
+let store=legacy_createStore(reducer);// создали стор
 export default store;
+window.store=store;
