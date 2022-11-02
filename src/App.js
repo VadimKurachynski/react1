733afd4209c61008bf30e860932b1d5663bc5036
VadimKurachynski <kurachynski@gmail.com>
2022-11-02 21:41:38 +0300

    6

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import './App.css';
 import Page from "./Page/Page";
 
+
 function App() {
   return (
     <div className="App">
-     < Page />
+     < Page  like="4"/>
     </div>
   );
 }
--- src/Page/Page.jsx
+++ src/Page/Page.jsx
@@ -1,19 +1,24 @@
 import s from "./Page.module.css"
+import {connect} from "react-redux";
 
-
-const PageStr = () => {
-
+const PageStr = (props) => {
+console.log("pagestr   ----   ",props);
     return (
         <div className={s.content}>
             <div>верх</div>
             <div><button>1</button><button>2</button></div>
-            <div></div>
+            <div>{props.like}</div>
             <div>низ</div>
         </div>
     )
 }
 
 
+function mapStateToProps(state){
 
+    return {
+        like:state.likes
+    }
+}
 
 export default PageStr;
